28e0b0c1d4470a58913d76eb188a1d066a90fd27
Corey Rowe <coreyr1196@gmail.com>
2018-08-10 01:27:38 -0400

    Update licensing and copyright year

--- src/main/java/com/gbsnowday/snowday/controller/SnowDayController.java
+++ src/main/java/com/gbsnowday/snowday/controller/SnowDayController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 - 2018 Corey Rowe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gbsnowday.snowday.controller;
 
 import com.gbsnowday.snowday.model.ClosingModel;
@@ -39,16 +55,6 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class SnowDayController {
-    /*Copyright 2014-2016 Corey Rowe
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-         http:www.apache.org/licenses/LICENSE-2.0
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.*/
 
     //Declare scene controls
     public Button btnCalculate;
